dca9765e6cbadf44f9d9834dccd8ec52b487a5e8
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-07-18 09:16:47 -0300

    separando as lógicas do arquivo server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const { v4: generateId } = require('uuid');
+const services = require('./server/services');
+const {
+  getFirstPlayer,
+  changeCurrentPlayer,
+  conditionsToWin,
+  getWinner,
+  isRepeatedPosition,
+} = services;
 
 // Inicializando o servidor e definindo a porta
 
@@ -17,51 +25,6 @@ const server = http.createServer(app);
 const router = express.Router();
 
 // Funções chamadas nas rotas
-
-const getFirstPlayer = () => (Math.random() >= 0.5 ? 'X' : 'O');
-
-const changeCurrentPlayer = (currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X');
-
-const conditionsToWin = (moves) => {
-  const conditionForDiagonalUp = moves.filter(({ x, y }) => x == y);
-  const conditionForDiagonalDown = moves.filter(({ x, y }) => x == 2 - y);
-
-  const conditionForColumn = moves.reduce(
-    (acc, { x }) => {
-      acc[x] = acc[x] + 1;
-      return acc;
-    },
-    [0, 0, 0],
-  );
-  const conditionForLine = moves.reduce(
-    (acc, { y }) => {
-      acc[y] = acc[y] + 1;
-      return acc;
-    },
-    [0, 0, 0],
-  );
-
-  if (conditionForDiagonalUp.length == 3 || conditionForDiagonalDown.length == 3) return true;
-  if (conditionForLine.indexOf(3) >= 0 || conditionForColumn.indexOf(3) >= 0) return true;
-  return false;
-};
-
-const getWinner = (movesByPlayerX, movesByPlayerO) => {
-  const playerO = conditionsToWin(movesByPlayerO);
-  const playerX = conditionsToWin(movesByPlayerX);
-
-  if (playerO) return 'O';
-  if (playerX) return 'X';
-};
-
-const isRepeatedPosition = (positionsPlayed, newPosition) => {
-  const { x: newX, y: newY } = newPosition;
-  const haveXplayed = positionsPlayed.X.some(({ x, y }) => x == newX && y == newY);
-  const haveOplayed = positionsPlayed.O.some(({ x, y }) => x == newX && y == newY);
-
-  if (haveOplayed) return true;
-  if (haveXplayed) return true;
-};
 
 const createNewGame = (player) => {
   return {
@@ -93,7 +56,7 @@ router.post('/game', (req, res) => {
 // Realizando movimentos
 
 router.post('/game/:id/movement', (req, res) => {
-  // const { id } = req.params;
+  // const { id } = req.params // Esse poderia ser outro caminho, havia uma ambiguidade em relação à utilização do id na proposta do projeto.
   const { player, position, id } = req.body;
 
   if (!games[id]) {
---
+++ server/services.js
@@ -0,0 +1,52 @@
+const getFirstPlayer = () => (Math.random() >= 0.5 ? 'X' : 'O');
+
+const changeCurrentPlayer = (currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X');
+
+const conditionsToWin = (moves) => {
+  const conditionForDiagonalUp = moves.filter(({ x, y }) => x == y);
+  const conditionForDiagonalDown = moves.filter(({ x, y }) => x == 2 - y);
+
+  const conditionForColumn = moves.reduce(
+    (acc, { x }) => {
+      acc[x] = acc[x] + 1;
+      return acc;
+    },
+    [0, 0, 0],
+  );
+  const conditionForLine = moves.reduce(
+    (acc, { y }) => {
+      acc[y] = acc[y] + 1;
+      return acc;
+    },
+    [0, 0, 0],
+  );
+
+  if (conditionForDiagonalUp.length == 3 || conditionForDiagonalDown.length == 3) return true;
+  if (conditionForLine.indexOf(3) >= 0 || conditionForColumn.indexOf(3) >= 0) return true;
+  return false;
+};
+
+const getWinner = (movesByPlayerX, movesByPlayerO) => {
+  const playerO = conditionsToWin(movesByPlayerO);
+  const playerX = conditionsToWin(movesByPlayerX);
+
+  if (playerO) return 'O';
+  if (playerX) return 'X';
+};
+
+const isRepeatedPosition = (positionsPlayed, newPosition) => {
+  const { x: newX, y: newY } = newPosition;
+  const haveXplayed = positionsPlayed.X.some(({ x, y }) => x == newX && y == newY);
+  const haveOplayed = positionsPlayed.O.some(({ x, y }) => x == newX && y == newY);
+
+  if (haveOplayed) return true;
+  if (haveXplayed) return true;
+};
+
+module.exports = {
+  getFirstPlayer,
+  changeCurrentPlayer,
+  conditionsToWin,
+  getWinner,
+  isRepeatedPosition
+}
